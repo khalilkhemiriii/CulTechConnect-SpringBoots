a3a8130d940b0f006aa058bb73fca26f6f12b0b4
khalilkhemiriii <mohamedkhalil.khemiri@esprit.tn>
2023-09-18 15:27:39 +0100

    1st commit spring boot

--- src/main/java/spring/licenta/config/WebSecurityConfig.java
+++ src/main/java/spring/licenta/config/WebSecurityConfig.java
@@ -23,8 +23,8 @@ import spring.licenta.services.UserDetailsServiceImpl;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
-		// securedEnabled = true,
-		// jsr250Enabled = true,
+		 securedEnabled = true,
+		 jsr250Enabled = true,
 		prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
--- src/main/java/spring/licenta/entities/ImageModel.java
+++ src/main/java/spring/licenta/entities/ImageModel.java
@@ -12,7 +12,6 @@ import javax.persistence.*;
 @Table(name="image_model")
 @AllArgsConstructor
 @Data
-@NoArgsConstructor
 @ToString
 public class ImageModel {
     @Id
